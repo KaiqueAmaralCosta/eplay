3020a1d990e2b547de8bdd2e737806ea4e3aeded
KaiqueAmaralCosta <KaiqueCosta.amaral@gmail.com>
2024-09-02 20:08:15 -0300

    criando a estrutura das secoes dos jogos indiviuais

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Categories from './pages/Categories'
+import Product from './pages/Product'
 
 // configuracao das todas de link
 
@@ -9,6 +10,7 @@ const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
